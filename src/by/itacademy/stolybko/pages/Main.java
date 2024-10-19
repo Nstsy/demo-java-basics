9119baa8a05075fef7b5cccd68f9c1b030d57665
nstsy <nstsy@yandex.ru>
2024-10-19 22:16:29 +0300

    add add changes to class Main

--- src/by/itacademy/stolybko/pages/Main.java
+++ src/by/itacademy/stolybko/pages/Main.java
@@ -5,5 +5,9 @@ public class Main {
         HomePage homePage = new HomePage();
         homePage.open();
         System.out.println("Site name: " + homePage.getTitle());
+        homePage.clickLogin();
+        LoginPage loginPage = new LoginPage();
+        loginPage.open();
+        System.out.println("Tiltle login form: " + loginPage.getTitle());
     }
 }
