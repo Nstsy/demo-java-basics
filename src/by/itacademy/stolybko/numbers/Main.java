f6add72bb742a33ccc1a012908024576133e76af
nstsy <nstsy@yandex.ru>
2024-10-19 21:08:32 +0300

    add task solution

--- src/by/itacademy/stolybko/numbers/Main.java
+++ src/by/itacademy/stolybko/numbers/Main.java
@@ -4,7 +4,40 @@ public class Main {
     public static void main(String[] args) {
         int number = 7;
         String numberAsAString = "";
-
+        switch (number) {
+            case 1:
+                numberAsAString = "one";
+                break;
+            case 2:
+                numberAsAString = "two";
+                break;
+            case 3:
+                numberAsAString = "three";
+                break;
+            case 4:
+                numberAsAString = "four";
+                break;
+            case 5:
+                numberAsAString = "five";
+                break;
+            case 6:
+                numberAsAString = "six";
+                break;
+            case 7:
+                numberAsAString = "seven";
+                break;
+            case 8:
+                numberAsAString = "eight";
+                break;
+            case 9:
+                numberAsAString = "nine";
+                break;
+            case 0:
+                numberAsAString = "zero";
+            default:
+                numberAsAString = "";
+        }
+        System.out.println(numberAsAString);
     }
 
 }
