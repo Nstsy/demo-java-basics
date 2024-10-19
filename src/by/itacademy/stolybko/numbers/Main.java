770dbea04dc82a693470bbf5e627d755bfd34873
nstsy <nstsy@yandex.ru>
2024-10-19 21:07:39 +0300

    add task data

--- src/by/itacademy/stolybko/numbers/Main.java
+++ src/by/itacademy/stolybko/numbers/Main.java
@@ -1,4 +1,10 @@
 package by.itacademy.stolybko.numbers;
 
 public class Main {
+    public static void main(String[] args) {
+        int number = 7;
+        String numberAsAString = "";
+
+    }
+
 }
