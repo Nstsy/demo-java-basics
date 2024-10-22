99cefa6f2784144743cf5e3e1c6d1fb4e47943b0
nstsy <nstsy@yandex.ru>
2024-10-22 22:18:10 +0300

    add tests

--- src/test/biz/svyatoslav/CalculatorTest.java
+++ src/test/biz/svyatoslav/CalculatorTest.java
@@ -12,14 +12,34 @@ public class CalculatorTest {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://svyatoslav.biz/testlab/wt/index.php");
 
-        String xpathName = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
-        By byName = By.xpath(xpathName);
-        WebElement webElementName = webDriver.findElement(byName);
-        webElementName.sendKeys("Ivan");
+        String inputNamexpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNamexby = By.xpath(inputNamexpath);
+        WebElement inputNamexWebElement = webDriver.findElement(inputNamexby);
+        inputNamexWebElement.sendKeys("Ivan");
 
-        String xpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
-        By by = By.xpath(xpath);
-        WebElement webElement = webDriver.findElement(by);
-        webElement.click();
+        String inputHightxpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHightby = By.xpath(inputHightxpath);
+        WebElement inputHightWebElement = webDriver.findElement(inputHightby);
+        inputHightWebElement.sendKeys("180");
+
+        String inputWeightxpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputWeightxby = By.xpath(inputWeightxpath);
+        WebElement inputWeightWebElement = webDriver.findElement(inputWeightxby);
+        inputWeightWebElement.sendKeys("70");
+
+        String inputSubmitxpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By inputSubmitby = By.xpath(inputSubmitxpath);
+        WebElement inputSubmitWebElement = webDriver.findElement(inputSubmitby);
+        inputSubmitWebElement.click();
+
+        String inputRadioxpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By inputRadioby = By.xpath(inputRadioxpath);
+        WebElement inputRadioWebElement = webDriver.findElement(inputRadioby);
+        inputRadioWebElement.click();
+
+
+
+
+
     }
 }
