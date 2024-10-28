8b27e6793269c441110eaf1430e7106bb0acc53f
nstsy <nstsy@yandex.ru>
2024-10-28 15:32:45 +0300

    add LoginTest class

---
+++ src/test/com/netflix/LoginTest.java
@@ -0,0 +1,10 @@
+package com.netflix;
+
+import org.junit.jupiter.api.Test;
+
+//Сайт - Форма логина https://www.netflix.com/login
+//пустые поля, одно из полей заполнено, оба поля заполнены незарегистрированным пользователем
+public class LoginTest {
+    @Test
+    public void
+}
